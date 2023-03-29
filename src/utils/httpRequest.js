ae944a71ef1bf73256f0b84ad359d11929b08138
huybk2 <huybk2@fpt.com.vn>
2023-03-29 19:07:19 +0700

    update

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 import tokenService from '../services/tokenService';
 
 const httpRequest = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://bkhuy.pythonanywhere.com/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
